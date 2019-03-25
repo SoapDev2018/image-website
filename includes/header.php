82f576ffab1324da226c50a769eb35a75c6e773d
Sayantan Chaudhuri <sayantandev2018@gmail.com>
2019-03-25 19:08:57 +0530

    Made nav bar

--- includes/header.php
+++ includes/header.php
@@ -13,8 +13,30 @@
 <html lang="en">
 <head>
   <meta charset="UTF-8">
+  <link rel="stylesheet" text="text/css" href="assets/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+  <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.8.1/css/all.css">
+  <link rel="stylesheet" href="https://static.fontawesome.com/css/fontawesome-app.css">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="assets/js/bootstrap.bundle.min.js"></script>
   <title>Project Redwood | Image Sharing Website</title>
 </head>
 <body>
+  <div class="top_bar">
+    <div class="logo">
+      <a href="index.php">Redwood</a>
+    </div>
+
+    <nav>
+      <a href="#">
+        <?php echo $user['first_name']; ?>
+      </a>
+      <a href="#"><i class="fal fa-home fa-lg"></i></a>
+      <a href="#"><i class="fal fa-envelope fa-lg"></i></a>
+      <a href="#"><i class="fal fa-bell fa-lg"></i></a>
+      <a href="#"><i class="fal fa-cog fa-lg"></i></a>
+      <a href="#"><i class="fal fa-sign-out-alt fa-lg"></i></a>
+    </nav>
+  </div>
